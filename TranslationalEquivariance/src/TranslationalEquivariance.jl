813bd4320c2e4d566b35b715f77f06ee246158b5
BenjaminHenriksson <abenjaminhenriksson@gmail.com>
2025-06-27 15:09:27 +0200

    done stuff

--- TranslationalEquivariance/src/TranslationalEquivariance.jl
+++ TranslationalEquivariance/src/TranslationalEquivariance.jl
@@ -1,7 +1,6 @@
 module TranslationalEquivariance
 
-include("multidimensional_rope.jl")
 include("learnableRoPE.jl")
-export multidimensional_rope, LearnableRoPE, RoPE
+export LearnableRoPE, RoPE
 
 end # module TranslationalEquivariance
