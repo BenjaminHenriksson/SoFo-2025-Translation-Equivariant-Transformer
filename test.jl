eb4cf682c1c0c83f03459c946023b2f6b3f84044
BenjaminHenriksson <abenjaminhenriksson@gmail.com>
2025-06-30 14:21:00 +0200

    done stuff

---
+++ test.jl
@@ -0,0 +1,51 @@
+using Pkg; Pkg.activate(".")
+using Flux, DLProteinFormats, Onion, RandomFeatureMaps, StatsBase, Plots
+
+dat = DLProteinFormats.load(PDBSimpleFlat500);
+
+L = 30
+train_inds = findall(dat.len .> L)
+ 
+function random_batch(dat, L, B, filt_inds)
+    locs = zeros(Float32, 3, L, B)
+    inds = sample(filt_inds, B, replace=false)
+    AAs = zeros(Int, L, B)
+    for (i,ind) in enumerate(inds)
+        l_range = rand(1:dat[ind].len - L + 1)
+        locs[:, :, i] = dat[ind].locs[:, 1, l_range:l_range+L-1]
+        AAs[:, i] = dat[ind].AAs[l_range:l_range+L-1]
+    end
+    return (;locs, AAs = Flux.onehotbatch(AAs, 1:20))
+end
+
+batch = random_batch(dat, L, 10, train_inds);
+ 
+struct Toy1{L}
+    layers::L
+end
+Flux.@layer Toy1
+function Toy1(dim, depth)
+    layers = (;
+        loc_encoder = Dense(3 => dim, bias=false),
+        transformers = [Onion.TransformerBlock(dim, 8, Onion.MultiDimRoPE) for _ in 1:depth],
+        AA_decoder = Dense(dim => 20, bias=false),
+    )
+    return Toy1(layers)
+end
+function (m::Toy1)(locs)
+    l = m.layers
+    x = l.loc_encoder(locs)
+    for transformerblock in l.transformers
+        x = transformerblock(x, 0, nothing, locs)
+        #locs = updatelocs(x, locs)
+    end
+    aa_logits = l.AA_decoder(x)
+    return aa_logits
+end
+
+model = Toy1(64, 4)
+opt_state = Flux.setup(AdamW(eta = 0.001), model)
+losses = Float32[]
+
+test = random_batch(dat, L, 1, train_inds)
+model(test.locs)
