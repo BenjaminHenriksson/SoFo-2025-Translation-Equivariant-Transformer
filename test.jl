e8d9ee5fc8c7433bedd17b7caf85fbfe015e701b
BenjaminHenriksson <abenjaminhenriksson@gmail.com>
2025-07-04 15:05:26 +0200

    Extensive debugging work being done to ensure translational invariance; issue source unknown.

--- test.jl
+++ test.jl
@@ -3,6 +3,8 @@ using Pkg; Pkg.activate(".")
 # Make sure the Dev version of Onion is loaded (should print "dev env" when loading)
 using Flux, Onion, Test
 
+using Random
+
 # These tests should actually be EQUIVARIANT, not INVARIANT!
 # Tests need to be rewritten to consider RoPE between tokens, not independently of postions!
 
