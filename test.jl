fff7ee0a81d3c177561330f3b98caaf8df0ef6b3
Benjamin Henriksson <abenjaminhenriksson@gmail.com>
2025-07-07 14:14:40 +0200

    STRING.jl test cases added

--- test.jl
+++ test.jl
@@ -1,10 +1,20 @@
 using Pkg; Pkg.activate(".")
 
 # Make sure the Dev version of Onion is loaded (should print "dev env" when loading)
-using Flux, Onion, Test
+using Onion, Test
 
-using Random
+@testset "STRING Equivariance" begin
+    dim = 8
+    d_coords = 2
+    rope = Onion.STRING(dim, d_coords)
 
-# These tests should actually be EQUIVARIANT, not INVARIANT!
-# Tests need to be rewritten to consider RoPE between tokens, not independently of postions!
+    for _ in 1:3
+        xi = randn(Float32, d_coords)
+        xj = randn(Float32, d_coords)
 
+        lhs = rope(xi)' * rope(xj)
+        rhs = rope(xj .- xi)
+
+        @test isapprox(lhs, rhs; rtol=1e-5, atol=1e-5)
+    end
+end
