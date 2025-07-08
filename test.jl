a56718ec80f8d5f45857c11d9fb9fd1854a5ae41
Benjamin Henriksson <abenjaminhenriksson@gmail.com>
2025-07-08 16:19:44 +0200

    Tests passing for STRING.jl

--- test.jl
+++ test.jl
@@ -1,24 +1,29 @@
-using Pkg; Pkg.activate(".")
+using Pkg; Pkg.activate("."); Pkg.instantiate()
 
-# Make sure the Dev version of Onion is loaded (should print "dev env" when loading)
-using Flux, Onion, Test, StatsBase
+using Test, Onion
 
-dat = DLProteinFormats.load(PDBSimpleFlat500);
-
-L = 30
-train_inds = findall(dat.len .> L)
- 
-function random_batch(dat, L, B, filt_inds)
-    locs = zeros(Float32, 3, L, B)
-    inds = sample(filt_inds, B, replace=false)
-    AAs = zeros(Int, L, B)
-    for (i,ind) in enumerate(inds)
-        l_range = rand(1:dat[ind].len - L + 1)
-        locs[:, :, i] = dat[ind].locs[:, 1, l_range:l_range+L-1]
-        AAs[:, i] = dat[ind].AAs[l_range:l_range+L-1]
-    end
-    return (;locs, AAs = Flux.onehotbatch(AAs, 1:20))
+module STRINGBackup
+    include(joinpath(@__DIR__, "Onion.jl", "src", "STRING_backup.jl"))
 end
 
-batch = random_batch(dat, L, 10, train_inds) # shape = (3, 30, 10) = (d_coords, seq_len, batches)
- 
+@testset "STRING Batched vs Non-Batched" begin
+    dim = 8
+    d_coords = 3
+    seq_len = 4
+    batch = 2
+
+    rope_batched = Onion.STRING(dim, d_coords)
+    rope_nonbatched = STRINGBackup.STRING(dim, d_coords)
+
+    positions = rand(Float32, d_coords, seq_len, batch)
+    batched_out = rope_batched(positions)
+
+    expected = Array{Float32}(undef, dim, seq_len, batch, dim)
+    for b in 1:batch
+        for s in 1:seq_len
+            expected[:, s, b, :] = rope_nonbatched(positions[:, s, b])
+        end
+    end
+
+    @test isapprox(batched_out, expected; rtol=1e-5, atol=1e-5)
+end
